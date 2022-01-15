d7d1219976fdee8ad7b302b8ebaa8f13797c8881
Серёга Петров <97385729+EgoisTa-Git@users.noreply.github.com>
2022-01-15 19:26:59 +0000

    fixing little bugs

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ email_to = 'egoistapig1989@gmail.com'
 friend_name = 'Alex'
 my_name = 'Серёга'
 website = 'https://dvmn.org/referrals/QoqUjm1J75qqIGZKnxXRFLTP39yORMF6AwOxAA4J/'
-letter = (f"""From: {email_from}
+letter = f"""From: {email_from}
 To: {email_to}
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";
@@ -29,7 +29,7 @@ Content-Type: text/plain; charset="UTF-8";
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → {website}  
-На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""")
+На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
 letter=letter.encode("UTF-8")
 
 server=smtplib.SMTP_SSL(smtp_server)
